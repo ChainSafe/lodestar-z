d69c29d8e07d152ac76d719a7faa4c36e948777e
Jeff Chung <sh1001309@gmail.com>
2026-01-08 17:21:13 +0800

    feat: add blinded block tests in int

--- src/state_transition/test_utils/generate_block.zig
+++ src/state_transition/test_utils/generate_block.zig
@@ -11,6 +11,10 @@ const ZERO_HASH = @import("constants").ZERO_HASH;
 const CachedBeaconStateAllForks = state_transition.CachedBeaconStateAllForks;
 const computeStartSlotAtEpoch = state_transition.computeStartSlotAtEpoch;
 const getBlockRootAtSlot = state_transition.getBlockRootAtSlot;
+const getExpectedWithdrawals = state_transition.getExpectedWithdrawals;
+const WithdrawalsResult = state_transition.WithdrawalsResult;
+const Withdrawals = types.capella.Withdrawals.Type;
+const ValidatorIndex = types.primitive.ValidatorIndex.Type;
 
 /// Generate a valid electra block for the given pre-state.
 pub fn generateElectraBlock(allocator: Allocator, cached_state: *const CachedBeaconStateAllForks, out: *types.electra.SignedBeaconBlock.Type) !void {
@@ -85,6 +89,103 @@ pub fn generateElectraBlock(allocator: Allocator, cached_state: *const CachedBea
                     .sync_committee_signature = types.primitive.BLSSignature.default_value,
                 },
                 .execution_payload = execution_payload,
+                .bls_to_execution_changes = types.capella.SignedBLSToExecutionChanges.default_value,
+                .blob_kzg_commitments = types.electra.BlobKzgCommitments.default_value,
+                .execution_requests = types.electra.ExecutionRequests.default_value,
+            },
+        },
+        .signature = types.primitive.BLSSignature.default_value,
+    };
+}
+
+/// Generate a valid electra blinded block for the given pre-state.
+pub fn generateElectraBlindedBlock(allocator: Allocator, cached_state: *const CachedBeaconStateAllForks, out: *types.electra.SignedBlindedBeaconBlock.Type) !void {
+    const state = cached_state.state;
+    var attestations = types.electra.Attestations.default_value;
+    // no need to fill up to MAX_ATTESTATIONS_ELECTRA
+    const att_slot: Slot = state.slot() - 2;
+    const att_index = 0;
+    const att_block_root = try getBlockRootAtSlot(state, att_slot);
+    const target_epoch = cached_state.getEpochCache().epoch;
+    const target_epoch_slot = computeStartSlotAtEpoch(target_epoch);
+    const att_data: types.phase0.AttestationData.Type = .{
+        .slot = att_slot,
+        .index = att_index,
+        .beacon_block_root = att_block_root,
+        .source = state.currentJustifiedCheckpoint().*,
+        .target = .{
+            .epoch = target_epoch,
+            .root = try getBlockRootAtSlot(state, target_epoch_slot),
+        },
+    };
+    const committee_count = try cached_state.getEpochCache().getCommitteeCountPerSlot(target_epoch);
+    var total_committee_size: usize = 0;
+    for (0..committee_count) |committee_index| {
+        const committee = try cached_state.getEpochCache().getBeaconCommittee(att_slot, committee_index);
+        total_committee_size += committee.len;
+    }
+
+    var aggregation_bits = try ssz.BitListType(preset.MAX_VALIDATORS_PER_COMMITTEE * preset.MAX_COMMITTEES_PER_SLOT).Type.fromBitLen(allocator, total_committee_size);
+    // TODO: why this does not work
+    // var aggregation_bits = @field(types.electra.Attestation.Fields, "aggregation_bits").Type.fromBitLen(allocator, total_committee_size);
+    for (0..total_committee_size) |i| {
+        try aggregation_bits.set(allocator, i, true);
+    }
+
+    var committee_bits = ssz.BitVectorType(preset.MAX_COMMITTEES_PER_SLOT).default_value;
+    // var committee_bits = @field(types.electra.Attestation.Fields, "committee_bits").default_value;
+    for (0..committee_count) |i| {
+        try committee_bits.set(i, true);
+    }
+
+    try attestations.append(allocator, .{
+        .aggregation_bits = aggregation_bits,
+        .data = att_data,
+        .signature = types.primitive.BLSSignature.default_value,
+        .committee_bits = committee_bits,
+    });
+
+    // Compute the expected withdrawals root to match what processWithdrawals expects
+    var withdrawals_result = WithdrawalsResult{ .withdrawals = try Withdrawals.initCapacity(
+        allocator,
+        preset.MAX_WITHDRAWALS_PER_PAYLOAD,
+    ) };
+    defer withdrawals_result.withdrawals.deinit(allocator);
+    var withdrawal_balances = std.AutoHashMap(ValidatorIndex, usize).init(allocator);
+    defer withdrawal_balances.deinit();
+    try getExpectedWithdrawals(allocator, &withdrawals_result, &withdrawal_balances, cached_state);
+
+    var withdrawals_root: Root = undefined;
+    try types.capella.Withdrawals.hashTreeRoot(allocator, &withdrawals_result.withdrawals, &withdrawals_root);
+
+    var execution_payload_header = types.electra.ExecutionPayloadHeader.default_value;
+    execution_payload_header.timestamp = 1737111896;
+    execution_payload_header.withdrawals_root = withdrawals_root;
+
+    out.* = .{
+        .message = .{
+            .slot = state.slot() + 1,
+            // value is generated after running real state transition int test
+            .proposer_index = 41,
+            .parent_root = try hex.hexToRoot("0x4e647394b6f96c1cd44938483ddf14d89b35d3f67586a59cbfd410a56efbb2b1"),
+            // this could be computed later
+            .state_root = [_]u8{0} ** 32,
+            .body = .{
+                .randao_reveal = [_]u8{0} ** 96,
+                .eth1_data = types.phase0.Eth1Data.default_value,
+                .graffiti = [_]u8{0} ** 32,
+                // BlindedBeaconBlockBody uses different list types than BeaconBlockBody
+                // Use getFieldType() to get the correct SSZ type and its default_value
+                .proposer_slashings = types.electra.BlindedBeaconBlockBody.getFieldType("proposer_slashings").default_value,
+                .attester_slashings = types.electra.BlindedBeaconBlockBody.getFieldType("attester_slashings").default_value,
+                .attestations = attestations,
+                .deposits = types.electra.BlindedBeaconBlockBody.getFieldType("deposits").default_value,
+                .voluntary_exits = types.electra.BlindedBeaconBlockBody.getFieldType("voluntary_exits").default_value,
+                .sync_aggregate = .{
+                    .sync_committee_bits = ssz.BitVectorType(preset.SYNC_COMMITTEE_SIZE).default_value,
+                    .sync_committee_signature = types.primitive.BLSSignature.default_value,
+                },
+                .execution_payload_header = execution_payload_header,
                 .bls_to_execution_changes = types.capella.SignedBLSToExecutionChanges.default_value,
                 .blob_kzg_commitments = types.electra.BlobKzgCommitments.default_value,
                 .execution_requests = types.electra.ExecutionRequests.default_value,
