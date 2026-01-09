ba879cbad02c8a3bda99f8ee91fa023f082ddb8c
Chen Kai <281165273grape@gmail.com>
2026-01-09 15:25:44 +0800

    fix: fix beacon state get const ptr
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/state_transition/utils/seed.zig
+++ src/state_transition/utils/seed.zig
@@ -57,7 +57,7 @@ pub fn getNextSyncCommitteeIndices(allocator: Allocator, state: *const BeaconSta
     const fork_seq = state.forkSeq();
     const rand_byte_count: ByteCount = if (fork_seq.gte(.electra)) ByteCount.Two else ByteCount.One;
     const max_effective_balance: u64 = if (fork_seq.gte(.electra)) preset.MAX_EFFECTIVE_BALANCE_ELECTRA else preset.MAX_EFFECTIVE_BALANCE;
-    const epoch = computeEpochAtSlot(state.slot()) + 1;
+    const epoch = computeEpochAtSlot(try state.slot()) + 1;
     var seed: [32]u8 = undefined;
     try getSeed(state, epoch, c.DOMAIN_SYNC_COMMITTEE, &seed);
     try computeSyncCommitteeIndices(allocator, &seed, active_indices, effective_balance_increments.items, rand_byte_count, max_effective_balance, preset.EFFECTIVE_BALANCE_INCREMENT, preset.SHUFFLE_ROUND_COUNT, out);
