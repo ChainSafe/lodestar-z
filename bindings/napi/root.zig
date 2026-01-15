ac58feb01267d2771fcfe8f736334ce0763a5528
Cayman <caymannava@gmail.com>
2026-01-15 17:38:26 -0500

    chore: add config binding

--- bindings/napi/root.zig
+++ bindings/napi/root.zig
@@ -7,4 +7,5 @@ comptime {
 fn register(env: napi.Env, exports: napi.Value) !void {
     try @import("./pool.zig").register(env, exports);
     try @import("./pubkey2index.zig").register(env, exports);
+    try @import("./index2pubkey.zig").register(env, exports);
 }
