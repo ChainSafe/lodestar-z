f180d8d931224222189125758aa12ee6ce513518
bing <spiralladder@fastmail.com>
2026-01-20 22:53:27 +0800

    Fix config

--- src/state_transition/block/process_execution_payload.zig
+++ src/state_transition/block/process_execution_payload.zig
@@ -30,6 +30,7 @@ pub fn processExecutionPayload(
     external_data: BlockExternalData,
 ) !void {
     const state = cached_state.state;
+    const beacon_config = cached_state.config;
     const epoch_cache = cached_state.getEpochCache();
     var partial_payload = PartialPayload{};
     switch (body) {
@@ -72,12 +73,12 @@ pub fn processExecutionPayload(
     // def compute_timestamp_at_slot(state: BeaconState, slot: Slot) -> uint64:
     //   slots_since_genesis = slot - GENESIS_SLOT
     //   return uint64(state.genesis_time + slots_since_genesis * SECONDS_PER_SLOT)
-    if (partial_payload.timestamp != try state.genesisTime() + try state.slot() * config.mainnet.chain_config.SECONDS_PER_SLOT) {
+    if (partial_payload.timestamp != (try state.genesisTime() + try state.slot() * beacon_config.chain.SECONDS_PER_SLOT)) {
         return error.InvalidExecutionPayloadTimestamp;
     }
 
     if (state.forkSeq().gte(.deneb)) {
-        const max_blobs_per_block = config.mainnet.config.getMaxBlobsPerBlock(computeEpochAtSlot(try state.slot()));
+        const max_blobs_per_block = beacon_config.getMaxBlobsPerBlock(computeEpochAtSlot(try state.slot()));
         if (body.blobKzgCommitmentsLen() > max_blobs_per_block) {
             return error.BlobKzgCommitmentsExceedsLimit;
         }
@@ -121,7 +122,8 @@ test "process execution payload - sanity" {
     defer test_state.deinit();
 
     var execution_payload: types.electra.ExecutionPayload.Type = types.electra.ExecutionPayload.default_value;
-    execution_payload.timestamp = try test_state.cached_state.state.genesisTime() + try test_state.cached_state.state.slot() * config.mainnet.chain_config.SECONDS_PER_SLOT;
+    const beacon_config = test_state.cached_state.config;
+    execution_payload.timestamp = try test_state.cached_state.state.genesisTime() + try test_state.cached_state.state.slot() * beacon_config.chain.SECONDS_PER_SLOT;
     var body: types.electra.BeaconBlockBody.Type = types.electra.BeaconBlockBody.default_value;
     body.execution_payload = execution_payload;
 
