1da17288434793632c605605a985310725f79509
bing <spiralladder@fastmail.com>
2026-05-21 16:12:17 +0800

    Revert "feat(blst): use external buffers for blst operations (#358)"
    This reverts commit 78e4678df2429cbd412956a14ebc4778438d3b32.

--- bindings/napi/blst.zig
+++ bindings/napi/blst.zig
@@ -139,10 +139,10 @@ pub const PublicKey = struct {
     pub fn toBytes(self: *const PublicKey, compress: ?js.Boolean) !js.Uint8Array {
         if (try boolOrDefault(compress, true)) {
             const bytes = self.raw.compress();
-            return js.Uint8Array.fromExternal(bytes[0..]);
+            return js.Uint8Array.from(bytes[0..]);
         }
         const bytes = self.raw.serialize();
-        return js.Uint8Array.fromExternal(bytes[0..]);
+        return js.Uint8Array.from(bytes[0..]);
     }
 
     pub fn toHex(self: *const PublicKey, compress: ?js.Boolean) !js.String {
@@ -220,10 +220,10 @@ pub const Signature = struct {
     pub fn toBytes(self: *const Signature, compress: ?js.Boolean) !js.Uint8Array {
         if (try boolOrDefault(compress, true)) {
             const bytes = self.raw.compress();
-            return js.Uint8Array.fromExternal(bytes[0..]);
+            return js.Uint8Array.from(bytes[0..]);
         }
         const bytes = self.raw.serialize();
-        return js.Uint8Array.fromExternal(bytes[0..]);
+        return js.Uint8Array.from(bytes[0..]);
     }
 
     pub fn toHex(self: *const Signature, compress: ?js.Boolean) !js.String {
@@ -301,9 +301,9 @@ pub const SecretKey = struct {
     }
 
     /// Serializes the SecretKey to bytes (32 bytes).
-    pub fn toBytes(self: *const SecretKey) !js.Uint8Array {
+    pub fn toBytes(self: *const SecretKey) js.Uint8Array {
         const bytes = self.raw.serialize();
-        return js.Uint8Array.fromExternal(bytes[0..]);
+        return js.Uint8Array.from(bytes[0..]);
     }
 
     pub fn toHex(self: *const SecretKey) !js.String {
