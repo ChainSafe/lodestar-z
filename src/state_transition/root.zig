bd4f0f5d01b5d6154b949af22de4d502f4dbeae2
Chen Kai <281165273grape@gmail.com>
2026-09-07 13:31:20 +0800

    fix: report proposer rewards from block transitions

--- src/state_transition/root.zig
+++ src/state_transition/root.zig
@@ -121,6 +121,7 @@ const weak_subjectivity = @import("weak_subjectivity.zig");
 pub const getLatestWeakSubjectivityCheckpointEpoch = weak_subjectivity.getLatestWeakSubjectivityCheckpointEpoch;
 
 test {
+    _ = @import("proposer_rewards_test.zig");
     _ = @import("memory_safety_test.zig");
     testing.refAllDecls(@This());
     testing.refAllDecls(seed);
