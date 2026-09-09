5d35494f154087d092f2eee2a37f0c47ee89fa46
bing <spiralladder@fastmail.com>
2026-09-09 21:14:18 +0800

    refactor(metrics): drop SSZ node-cache metrics (#666)
    Persistent tree state doesn't have a need for the
    `lodestar_stfn_{balances,validators}_nodes_populated_{hit,miss}_total`
    metrics. These metrics are a no-op, so we just remove them.

--- src/state_transition/state_transition.zig
+++ src/state_transition/state_transition.zig
@@ -179,8 +179,6 @@ pub fn stateTransition(
         allocator.destroy(post_cached_state);
     }
 
-    try metrics.state_transition.onStateClone(post_cached_state, .state_transition);
-
     try processSlots(
         allocator,
         io,
